31ebb3c31ce276a7113e18c23ab43393d5021b92
Radoslaw <merynos94@gmail.com>
2024-08-16 14:25:01 +0200

    Add DB (postgres) and configure.

--- kettlebell_app/kettlebell_app/settings.py
+++ kettlebell_app/kettlebell_app/settings.py
@@ -74,11 +74,14 @@ WSGI_APPLICATION = "kettlebell_app.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+       'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'OPTIONS': {
+               'service': 'kb_app_service',
+               'passfile': '.my_pgpass',
+           },
+       }
+   }
 
 
 # Password validation
