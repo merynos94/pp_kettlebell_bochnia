d68e520f4dac3e042c1bc866ba0f0cdfd867b8c0
Radoslaw <merynos94@gmail.com>
2024-09-01 23:15:08 +0200

    Refactor - Extend Table on Django panel admin

--- kettlebell_app/tournament/admin.py
+++ kettlebell_app/tournament/admin.py
@@ -5,7 +5,8 @@ from .resources import PlayerImportResource, PlayerExportResource
 
 class PlayerAdmin(ImportExportModelAdmin):
     resource_class = PlayerImportResource
-    list_display = ('name', 'surname', 'club', 'get_categories')
+    list_display = ('name', 'surname', 'weight', 'club', 'get_categories', 'snatch_kettlebell_weight', 'snatch_repetitions','tgu_weight','see_saw_press_weight_left','see_saw_press_weight_right','kb_squat_weight','tiebreak')
+    list_filter = ('club', 'categories', 'tiebreak')
     search_fields = ('name', 'surname', 'club__name', 'categories__name')
 
     def get_categories(self, obj):
