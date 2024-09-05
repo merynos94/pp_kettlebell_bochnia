098496ee8c6d0a4e377e005c13af1fd936559862
lchojnacki <lukaszchojnacki2@gmail.com>
2024-09-05 23:08:16 +0200

    Adjust prod settings

--- kettlebell_app/kettlebell_app/settings/prod.py
+++ kettlebell_app/kettlebell_app/settings/prod.py
@@ -1,8 +1,19 @@
 from kettlebell_app.settings.base import *
 
+DEBUG = False
+
+SECRET_KEY = "iyn*xn!13gvy5hc#5!q)r1b^8=x025$nav_^e%hmwlq+26r0o2"
+
+ALLOWED_HOSTS = ["ppkettlebell.toadres.pl", "localhost"]
+
+LOG_PATH = "../logs"
+DBBACKUP_PATH = "../dbbackup"
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": "db.sqlite3",
     }
 }
+
+STATIC_ROOT = BASE_DIR.joinpath("public")
